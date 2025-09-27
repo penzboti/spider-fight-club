7967228a005e438cfaf346f4891a8630c6ddd328
CsSzili <szilard.csutora@gmail.com>
2025-09-27 14:49:03 +0200

    leg

--- scenes/character_body_2d.gd
+++ scenes/character_body_2d.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
+var speed = 0
 
 
 func _physics_process(_delta: float) -> void:
@@ -10,14 +10,24 @@ func _physics_process(_delta: float) -> void:
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction_x := Input.get_axis("ui_left", "ui_right")
 	if direction_x:
-		velocity.x = direction_x * SPEED
+		velocity.x = direction_x * speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, speed)
 	
 	var direction_y := Input.get_axis("ui_up", "ui_down")
 	if direction_y:
-		velocity.y = direction_y * SPEED
+		velocity.y = direction_y * speed
 	else:
-		velocity.y = move_toward(velocity.y, 0, SPEED)
+		velocity.y = move_toward(velocity.y, 0, speed)
+	
+	if direction_x!=0 and direction_y!=0:
+		velocity.x /= 2**0.5
+		velocity.y /= 2**0.5
 
 	move_and_slide()
+
+func _on_leg_pressed() -> void:
+	speed += 100
+
+func _on_hand_pressed() -> void:
+	print("Handy")
