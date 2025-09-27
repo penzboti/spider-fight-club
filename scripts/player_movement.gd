1c244781bb64da71566c0a3091a0279df6264310
CsSzili <szilard.csutora@gmail.com>
2025-09-27 17:27:15 +0200

    Multiply by delta

--- scripts/player_movement.gd
+++ scripts/player_movement.gd
@@ -15,18 +15,18 @@ var hp_label
 func _on_ready() -> void:
 	set_hp(hp)  # Set initial HP value
 
-func _physics_process(_delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction_x := Input.get_axis("ui_left", "ui_right")
 	if direction_x:
-		velocity.x = direction_x * speed
+		velocity.x = direction_x * speed * delta
 	else:
 		velocity.x = move_toward(velocity.x, 0, speed)
 	
 	var direction_y := Input.get_axis("ui_up", "ui_down")
 	if direction_y:
-		velocity.y = direction_y * speed
+		velocity.y = direction_y * speed * delta
 	else:
 		velocity.y = move_toward(velocity.y, 0, speed)
 	
@@ -37,7 +37,7 @@ func _physics_process(_delta: float) -> void:
 	move_and_slide()
 
 func _on_leg_pressed() -> void:
-	speed += 100  # Get speed from leg
+	speed += 5000  # Get speed from leg
 	set_hp(hp-1)  # Buy leg with hp
 	# If hp is 0, die.
 	if hp<=0:  # Should not be less than 0, but who knows...
