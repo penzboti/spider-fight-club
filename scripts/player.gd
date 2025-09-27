91ad9defae9979e4d8562d6ed2dfea7fb54d6ae8
penzboti <penzboti@gmail.com>
2025-09-27 15:02:50 +0200

    scripts restructure, zip file, spawning players

---
+++ scripts/player.gd
@@ -0,0 +1,8 @@
+extends Node2D
+
+
+@export var number: int = 0
+
+
+func _ready() -> void:
+	print(number)
---
+++ scripts/player_movement.gd
@@ -0,0 +1,33 @@
+extends CharacterBody2D
+
+
+var speed = 0
+
+
+func _physics_process(_delta: float) -> void:
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction_x := Input.get_axis("ui_left", "ui_right")
+	if direction_x:
+		velocity.x = direction_x * speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed)
+	
+	var direction_y := Input.get_axis("ui_up", "ui_down")
+	if direction_y:
+		velocity.y = direction_y * speed
+	else:
+		velocity.y = move_toward(velocity.y, 0, speed)
+	
+	if direction_x!=0 and direction_y!=0:
+		velocity.x /= 2**0.5
+		velocity.y /= 2**0.5
+
+	move_and_slide()
+
+func _on_leg_pressed() -> void:
+	speed += 100
+
+func _on_hand_pressed() -> void:
+	print("Handy")
