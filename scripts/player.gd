51623f53e19400947715fb9942076ab608bc15d8
csecsoliver <csecs.oliver.elemer@students.jedlik.eu>
2025-09-28 09:55:09 +0200

    arm damage + cooldown

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,5 +1,4 @@
 extends Node2D
-
 @export var index: int;
 var data: PlayerData
 
@@ -11,7 +10,7 @@ var legInstances: Dictionary = {}
 var armInstances: Dictionary = {}
 
 func display_hp():
-	$CharacterBody2D/Control/Red_hp_bar.position = $CharacterBody2D.position + Vector2(-max_hp*20, -160)
+	$CharacterBody2D/Control/Red_hp_bar.position =  Vector2(-max_hp*20, -160)
 	$CharacterBody2D/Control/Red_hp_bar.scale.x = max_hp
 	$CharacterBody2D/Control/Green_hp_bar.scale.x = hp
 	$CharacterBody2D/Control/Green_hp_bar.position = $CharacterBody2D/Control/Red_hp_bar.position
@@ -26,19 +25,29 @@ func _ready() -> void:
 	if not data.lives_changed.is_connected(_on_lives_changed):
 		data.lives_changed.connect(_on_lives_changed)
 
+	for i in range(data.legs):
+		var leg = spawn_limb("leg")
+		leg.identifier = "{data.id}_leg_{i}"
+	for i in data.arms:
+		var arm = spawn_limb("arm")
+		arm.identifier = "{data.id}_arm_{i}"
+
+	set_meta("id", data.id)
+	$CharacterBody2D.set_meta("id", data.id)
+
 
 func _process(_delta: float) -> void:
 	display_hp()
 
-func take_damage() -> void:
-	data.lose_life(1)
+func take_damage(i: int = 1) -> void:
+	data.lose_life(i)
 
 
 func _on_lives_changed(v: int) -> void:
 	hp = v
 	#display_hp()
 	
-func spawn_limb(type) -> bool:
+func spawn_limb(type) -> Node2D:
 	var filled_slots = legInstances.keys()
 	filled_slots.append_array(armInstances.keys())
 	filled_slots.shuffle()
@@ -48,7 +57,7 @@ func spawn_limb(type) -> bool:
 			available_slots.append(i)
 	if available_slots.size() == 0:
 		print("No available limb slots")
-		return false
+		return null
 	match type:
 		"leg":
 			var new_leg = limb.instantiate()
@@ -57,14 +66,29 @@ func spawn_limb(type) -> bool:
 			legInstances[available_slots[0]] = new_leg
 
 			add_child(new_leg)
-			return true
+			return new_leg
 		"arm":
 			var new_arm = limb.instantiate()
 			new_arm.base("arm", Color(0.2, 0.8, 0.2, 1), 4, 18, 5, %LimbAttachPoints.get_child(available_slots[0]))
 			armInstances[available_slots[0]] = new_arm
 			add_child(new_arm)
-			return true
+			return new_arm
 
 		_:
 			print("Unknown limb type: " + str(type))
-			return false
+			return null
+
+func handle_arm_collision(arm: Node2D, body: Node) -> bool:
+	# Called when an arm collides with something
+
+	if body.has_meta("id") and body.get_meta("id") != self.get_meta("id"):
+		var other_player = body
+		other_player.get_parent().take_damage(1)
+		data.arms[int(arm.identifier[-1])] -= 1
+		if data.arms[int(arm.identifier[-1])] <= 0:
+			data.arms.remove_at(int(arm.identifier[-1]))
+			arm.queue_free()
+			armInstances.erase(int(arm.identifier[-1]))
+		return true
+	else:
+		return false
